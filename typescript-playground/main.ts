fd872c832b012a007379ca0073307953deb4431b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-01-21 14:11:05 +0100

    ts: full example

--- typescript-playground/main.ts
+++ typescript-playground/main.ts
@@ -3,24 +3,22 @@ import { Test } from "./test";
 const test = new Test(42);
 
 
+const fn = (book: Book, { title }: Book) => 
+  ({ 
+    ...book,
+    title
+  });
+
 interface Book {
     title: string;
 }
 
-interface Book2 {
-    title: string;
-    year: number;
-}
 
 const book = {
     title: 'Angular',
     year: 2020
 };
 
+const result = fn(book, { title: 'React' });
 
-function test2(b: Book2) {
-    console.log(b);
-}
-
-// gültiger Aufruf
-test2(book);
+console.table(result);
