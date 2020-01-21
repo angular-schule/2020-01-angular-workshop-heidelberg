e1382c565f08e417f673c9b09214d6e633541a19
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-01-21 16:43:06 +0100

    introducing BookComponent

---
+++ book-rating/src/app/book/book.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BookComponent {
+
+  @Input() book: Book;
+}
