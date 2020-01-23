d7661c8c40f10735bc71893020dc8167d84c71b8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-01-23 14:07:55 +0100

    html for form

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { BookComponent } from './book/book.component';
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { BookComponent } from './book/book.component';
+import { CreateBookComponent } from './create-book/create-book.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    CreateBookComponent
   ],
   exports: [
     DashboardComponent
   ],
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class BooksModule { }
---
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'br-create-book',
+  templateUrl: './create-book.component.html',
+  styleUrls: ['./create-book.component.scss']
+})
+export class CreateBookComponent {
+
+  bookForm = new FormGroup({
+    isbn: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    title: new FormControl('', Validators.required),
+    description: new FormControl('')
+  });
+
+}
