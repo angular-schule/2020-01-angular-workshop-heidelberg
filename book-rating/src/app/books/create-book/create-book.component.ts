955c43f00aa91f2ecda34d5aa64c046287c61066
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-01-23 15:01:49 +0100

    forms: hands on!

--- book-rating/src/app/books/create-book/create-book.component.ts
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -17,4 +17,24 @@ export class CreateBookComponent {
     description: new FormControl('')
   });
 
+  isInvalid(name: string) {
+    const control = this.bookForm.get(name);
+    return control.invalid && control.touched;
+  }
+
+  submitForm() {
+    const newBook = {
+      ...this.bookForm.value,
+      rating: 1
+    };
+
+    // Hands On!
+    // 1. erstelle den EventEmitter create
+    // 2. versende ein Buch mit create
+    // 3. subscribe auf das Event im Dashboard (HTML!)
+    // 4. füge dem Books-Array das neue Buch hinzu!
+
+    this.bookForm.reset();
+  }
+
 }
