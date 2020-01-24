ce9b7e077d1e7f6218e2b32239783c8a0651e002
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-01-24 13:38:23 +0100

    rxjs: anti-pattern -> multiple subscribe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { interval } from 'rxjs';
+import { map, switchMap, catchError, retry } from 'rxjs/operators';
+import { BookStoreService } from '../shared/book-store.service';
+
 
 @Component({
   selector: 'br-book-details',
@@ -11,14 +14,15 @@ export class BookDetailsComponent implements OnInit {
 
   isbn: string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
 
   ngOnInit() {
-    this.route.paramMap
-      .subscribe(paramMap => this.isbn = paramMap.get('isbn'));
-
-    interval(500).subscribe(i => console.log(i));
-
+    this.route.paramMap.pipe(
+      map(paramMap => paramMap.get('isbn')),
+      map(isbn => this.bs.getSingle(isbn))
+    )
+    .subscribe(book$ => book$
+      .subscribe(book => this.isbn = book.title));
   }
 
 
