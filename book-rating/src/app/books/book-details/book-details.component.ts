ef0bb4a13d320d69a2554caaacddc7ac09d1d35c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-01-24 14:59:42 +0100

    rxjs: error-handling done right!

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { interval } from 'rxjs';
+import { interval, of } from 'rxjs';
 import { map, switchMap, catchError, retry, mergeMap, concatMap, share, shareReplay } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -14,7 +15,13 @@ export class BookDetailsComponent implements OnInit {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(x => this.bs.getSingle(x)),
+    switchMap(x => this.bs.getSingle(x).pipe(
+      catchError((err: HttpErrorResponse) => of({
+        isbn: '000',
+        title: 'Fehler beim Laden von ' + err.url,
+        description: 'Ups!'
+      }))
+    ))
   );
 
   showDetails = false;
