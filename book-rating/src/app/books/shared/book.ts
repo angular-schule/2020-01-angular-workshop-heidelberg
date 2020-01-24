dc6446b64fc3720ed73a236a30871d661cd40b8b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-01-24 09:49:12 +0100

    hands on solution: thumbnails

--- book-rating/src/app/books/shared/book.ts
+++ book-rating/src/app/books/shared/book.ts
@@ -1,6 +1,23 @@
+export interface Thumbnail {
+  url: string;
+  title: string;
+}
+
 export interface Book {
   isbn: string;
   title: string;
   description: string;
   rating: number;
+
+  // A - Liste von Thubnails (ohne eigenes Interface)
+  // thumbnails: {
+  //   url: string;
+  //   title: string;
+  // }[];
+
+  // B
+  // thumbnails: Thumbnail[];
+
+  // C
+  firstThumbnailUrl: string;
 }
